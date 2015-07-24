56e0f65fe57bccb36296e12f37829530f7e5edac
root <root@localhost.localdomain>
2015-07-25 02:59:03 +0800

    update

--- spider.lisp
+++ spider.lisp
@@ -1,15 +1,20 @@
 (ql:quickload :drakma)
 (ql:quickload :cl-ppcre)
-
+(ql:quickload :closure-html)
+(ql:quickload :cxml-stp)
 
 (defun sliver (page)
   (handler-case (drakma:http-request page :connection-timeout 5)
     (condition nil)))
 
 (defun picklinks (content)
-  (mapcar #'(lambda (string) 
-	      (subseq string 9 (- (length string) 1)))
-	  (cl-ppcre:all-matches-as-strings "<a href=\"http[^\"]*\"" content)))
+  (let ((result (list "result")))
+    (when (not (null content))
+      (stp:do-recursively (a (chtml:parse content (cxml-stp:make-builder)))
+	(when (and (typep a 'stp:element)
+		   (equal (stp:local-name a) "a"))
+	  (nconc result (cons (stp:attribute-value a "href") nil)))))
+    (cdr result)))
 
 
 (defun create-breed-spider (url)
